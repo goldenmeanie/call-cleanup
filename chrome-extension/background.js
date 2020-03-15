ee73eb6e3fe91747393b4751e49bba0642fe1048
AlpacaFur <luke@bigring.org>
2020-03-15 10:53:33 -0400

    Add wildcard functionality

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,6 @@
+"use strict";
 const URLS = ["zoom.us/postattendee"];
-const DOMAINS = [];
+const DOMAINS = ["e*e.com"];
 
 function urlIsOnBlacklist(url, blacklist) {
   let parsed = (url.startsWith("http://") || url.startsWith("https://")) ? url : "https://" + url;
@@ -11,25 +12,30 @@ function urlIsOnBlacklist(url, blacklist) {
     console.error(e);
     return false;
   }
-  if (DOMAINS.includes(parsed.host) ||
-      URLS.includes(parsed.host + parsed.pathname) ||
-      URLS.includes(parsed.host + parsed.pathname + parsed.hash))
-      return true;
-  else return false;
+  if (bulkWildcardTest(parsed.host, DOMAINS) ||
+      bulkWildcardTest(parsed.host + parsed.pathname, URLS) ||
+      bulkWildcardTest(parsed.host + parsed.pathname + parsed.hash, URLS)) {
+
+    return true;
+  }
+  else {
+
+  }
+
+  return false;
 }
 
-// Auto-close forbidden tabs
-//
-// chrome.tabs.onUpdated.addListener((tabId, change, tab)=>{
-//
-//   // Prevents finished loading from throwing an error when trying to redelete a deleted tab
-//   if (!change.url) {
-//     console.log("event discarded")
-//     return;
-//   }
-//
-//   if (tabIsOnBlacklist(tab.url)) chrome.tabs.close(tabId);
-// })
+function bulkWildcardTest(input, wildcards) {
+  return wildcards.some((wildcard)=>{
+    return wildcardTest(input, wildcard)
+  })
+}
+
+// https://stackoverflow.com/a/32402438
+function wildcardTest(input, pattern) {
+  var escapeRegex = (input) => input.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  return new RegExp("^" + pattern.split("*").map(escapeRegex).join(".*") + "$").test(input);
+}
 
 function closeBadTabs() {
   chrome.tabs.query({}, (tabs)=>{
